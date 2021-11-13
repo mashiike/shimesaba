ac4a3cba6427f75b9c6a2a6eaa04c292e5caa333
mashiike <ikeda-masashi@kayac.com>
2021-11-13 12:41:16 +0900

    use urfave/cli/v2 for subcommands

--- cmd/shimesaba/main.go
+++ cmd/shimesaba/main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"context"
-	"flag"
+	"errors"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
-	"runtime"
+	"sort"
 	"strings"
 	"syscall"
 
@@ -15,30 +15,12 @@ import (
 	"github.com/handlename/ssmwrap"
 	"github.com/mashiike/shimesaba"
 	"github.com/mashiike/shimesaba/internal/logger"
+	"github.com/urfave/cli/v2"
 )
 
-type stringSlice []string
-
-func (i *stringSlice) String() string {
-	return fmt.Sprintf("%v", *i)
-}
-func (i *stringSlice) Set(v string) error {
-	if strings.ContainsRune(v, ',') {
-		*i = append(*i, strings.Split(v, ",")...)
-	} else {
-		*i = append(*i, v)
-	}
-	return nil
-}
-
 var (
-	Version        = "current"
-	mackerelAPIKey string
-	debug          bool
-	dryRun         bool
-	backfill       uint
-	version        bool
-	configFiles    stringSlice
+	Version = "current"
+	app     *shimesaba.App
 )
 
 func main() {
@@ -50,72 +32,115 @@ func main() {
 			Retries: 3,
 		})
 	}
+	cliApp := &cli.App{
+		Name:  "shimesaba",
+		Usage: "A commandline tool for tracking SLO/ErrorBudget using Mackerel as an SLI measurement service.",
+		Flags: []cli.Flag{
+			&cli.StringSliceFlag{
+				Name:     "config",
+				Aliases:  []string{"c"},
+				Usage:    "config file path, can set multiple",
+				Required: true,
+				EnvVars:  []string{"CONFIG", "SHIMESABA_CONFIG"},
+			},
+			&cli.StringFlag{
+				Name:        "mackerel-apikey",
+				Aliases:     []string{"k"},
+				Usage:       "for access mackerel API",
+				Required:    true,
+				DefaultText: "*********",
+				EnvVars:     []string{"MACKEREL_APIKEY", "SHIMESABA_MACKEREL_APIKEY"},
+			},
+			&cli.BoolFlag{
+				Name:    "debug",
+				Usage:   "output debug log",
+				EnvVars: []string{"SHIMESABA_DEBUG"},
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:  "run",
+				Usage: "run shimesaba. this is main feature",
+				Action: func(c *cli.Context) error {
+					if c.Int("backfill") <= 0 {
+						return errors.New("backfill count must positive value")
+					}
+					opts := []shimesaba.RunOption{
+						shimesaba.DryRunOption(c.Bool("dry-run")),
+						shimesaba.BackfillOption(c.Int("backfill")),
+					}
+					handler := func(ctx context.Context) error {
+						return app.Run(ctx, opts...)
+					}
+					if isLabmda() {
+						lambda.Start(handler)
+						return nil
+					}
+					return handler(c.Context)
+				},
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "dry-run",
+						Usage:   "report output stdout and not put mackerel",
+						EnvVars: []string{"SHIMESABA_DRY_RUN"},
+					},
+					&cli.IntFlag{
+						Name:        "backfill",
+						DefaultText: "3",
+						Value:       3,
+						Usage:       "generate report before n point",
+						EnvVars:     []string{"BACKFILL", "SHIMESABA_BACKFILL"},
+					},
+				},
+			},
+		},
+	}
+	sort.Sort(cli.FlagsByName(cliApp.Flags))
+	sort.Sort(cli.CommandsByName(cliApp.Commands))
+	cliApp.Version = Version
+	cliApp.EnableBashCompletion = true
+	cliApp.Before = func(c *cli.Context) error {
+		minLevel := "info"
+		if c.Bool("debug") {
+			minLevel = "debug"
+		}
+		logger.Setup(os.Stderr, minLevel)
+		switch c.Args().First() {
+		case "help", "h", "version":
+			return nil
+		default:
+		}
+		if ssmwrapErr != nil {
+			return fmt.Errorf("ssmwrap.Export failed: %w", ssmwrapErr)
+		}
+		cfg := shimesaba.NewDefaultConfig()
+		if err := cfg.Load(c.StringSlice("config")...); err != nil {
+			return err
+		}
+		if err := cfg.ValidateVersion(Version); err != nil {
+			return err
+		}
+		var err error
+		app, err = shimesaba.New(c.String("mackerel-apikey"), cfg)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
 
-	flag.Var(&configFiles, "config", "config file path, can set multiple")
-	flag.StringVar(&mackerelAPIKey, "mackerel-apikey", "", "for access mackerel API")
-	flag.BoolVar(&debug, "debug", false, "output debug log")
-	flag.BoolVar(&dryRun, "dry-run", false, "report output stdout and not put mackerel")
-	flag.BoolVar(&version, "version", false, "show version")
-	flag.UintVar(&backfill, "backfill", 3, "generate report before n point")
-	flag.VisitAll(envToFlag)
-	flag.Parse()
-
-	minLevel := "info"
-	if debug {
-		minLevel = "debug"
-	}
-	logger.Setup(os.Stderr, minLevel)
-	if version {
-		log.Printf("[info] shimesaba version : %s", Version)
-		log.Printf("[info] go runtime version: %s", runtime.Version())
-		return
-	}
-	if ssmwrapErr != nil {
-		logger.Setup(os.Stderr, "info")
-		log.Printf("[error] ssmwrap.Export failed: %s\n", ssmwrapErr)
-		os.Exit(1)
-	}
-	if backfill == 0 {
-		log.Println("[error] backfill count must positive avlue")
-		os.Exit(1)
-	}
-	cfg := shimesaba.NewDefaultConfig()
-	if err := cfg.Load(configFiles...); err != nil {
-		log.Println("[error]", err)
-		os.Exit(1)
-	}
-	if err := cfg.ValidateVersion(Version); err != nil {
-		log.Println("[error]", err)
-		os.Exit(1)
-	}
-	app, err := shimesaba.New(mackerelAPIKey, cfg)
-	if err != nil {
-		log.Println("[error]", err)
-		os.Exit(1)
-	}
-
-	if strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") ||
-		os.Getenv("AWS_LAMBDA_RUNTIME_API") != "" {
-		lambda.Start(lambdaHandler(app))
-		return
+	if isLabmda() {
+		if len(os.Args) <= 1 {
+			os.Args = append(os.Args, "run")
+		}
 	}
 	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
 	defer cancel()
-	if err := app.Run(ctx, shimesaba.DryRunOption(dryRun), shimesaba.BackfillOption(int(backfill))); err != nil {
-		log.Println("[error]", err)
-		os.Exit(1)
+	if err := cliApp.RunContext(ctx, os.Args); err != nil {
+		log.Printf("[error] %s", err)
 	}
 }
 
-func lambdaHandler(app *shimesaba.App) func(context.Context) error {
-	return func(ctx context.Context) error {
-		return app.Run(ctx, shimesaba.DryRunOption(dryRun), shimesaba.BackfillOption(int(backfill)))
-	}
-}
-
-func envToFlag(f *flag.Flag) {
-	name := strings.ToUpper(strings.Replace(f.Name, "-", "_", -1))
-	if s, ok := os.LookupEnv(name); ok {
-		f.Value.Set(s)
-	}
+func isLabmda() bool {
+	return strings.HasPrefix(os.Getenv("AWS_EXECUTION_ENV"), "AWS_Lambda") ||
+		os.Getenv("AWS_LAMBDA_RUNTIME_API") != ""
 }
