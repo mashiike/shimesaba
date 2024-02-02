995c3f5b11ed1bed391af3d00743a6bd68a06b14
mashiike <ikeda-masashi@kayac.com>
2024-02-02 12:11:25 +0900

    add test

--- mackerel_test.go
+++ mackerel_test.go
@@ -63,3 +63,46 @@ func TestRepositoryFetchVirtualAlerts(t *testing.T) {
 		})
 	}
 }
+
+func TestRepositoryFetchAlerts(t *testing.T) {
+	client := newMockMackerelClient(t)
+	repo := shimesaba.NewRepository(client)
+
+	cases := []struct {
+		name     string
+		startAt  time.Time
+		endAt    time.Time
+		expected shimesaba.Alerts
+	}{
+		{
+			name:    "Alerts service",
+			startAt: time.Date(2021, 10, 1, 0, 5, 0, 0, time.UTC),
+			endAt:   time.Date(2021, 10, 1, 0, 15, 0, 0, time.UTC),
+			expected: shimesaba.Alerts{
+				{
+					OpenedAt: time.Date(2021, 10, 1, 0, 10, 0, 0, time.UTC),
+					Monitor:  shimesaba.NewMonitor("dummyMonitorID", "Dummy Service Metric Monitor", "service"),
+					ClosedAt: ptrTime(time.Date(2021, 10, 1, 0, 15, 0, 0, time.UTC)),
+				},
+			},
+		},
+		{
+			name:     "No alerts",
+			startAt:  time.Date(2022, 10, 1, 0, 5, 0, 0, time.UTC),
+			endAt:    time.Date(2022, 10, 1, 0, 15, 0, 0, time.UTC),
+			expected: shimesaba.Alerts{},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			alerts, err := repo.FetchAlerts(context.Background(), c.startAt, c.endAt)
+			require.NoError(t, err)
+			for _, a := range alerts {
+				if a.Monitor != nil {
+					a.Monitor = a.Monitor.WithEvaluator(nil)
+				}
+			}
+			require.EqualValues(t, c.expected, alerts)
+		})
+	}
+}
